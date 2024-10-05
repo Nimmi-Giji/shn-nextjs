db7d55069304661e0824e809062f0dc454a0874a
NiranManoj <niranjanaamanoj@gmail.com>
2024-10-06 01:01:10 +0530

    finally api ready

--- quick-learn/app/api/generate/route.js
+++ quick-learn/app/api/generate/route.js
@@ -1,35 +1,42 @@
-import { NextResponse } from 'next/server'
-import OpenAI from 'openai'
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { NextResponse } from "next/server";
 
-const systemPrompt = `
-You are a flashcard creator, you take in text and create multiple flashcards from it. Make sure to create exactly 10 flashcards.
-Both front and back should be one sentence long.
-You should return in the following JSON format:
+const systemPrompt = `You are a flashcard generator. Given a TOPIC ON ADDITION, create exactly 10 flashcards. 
+Each flashcard should feature a term or concept related to the given topic on the front, and a brief explanation or definition of that term/concept on the back. 
+Ensure that the flashcards cover different important aspects or key details related to the topic.
+Return the flashcards in the following JSON format:
 {
   "flashcards":[
     {
-      "front": "Front of the card",
-      "back": "Back of the card"
+      "front": "term or concept",
+      "back": "short explanation or definition"
     }
   ]
-}
-`
+}`;
+
 export async function POST(req) {
-    const openai = new OpenAI()
-    const data = await req.text()
-  
-    const completion = await openai.chat.completions.create({
-      messages: [
-        { role: 'system', content: systemPrompt },
-        { role: 'user', content: data },
-      ],
-      model: 'gpt-4o',
-      response_format: { type: 'json_object' },
-    })
-  
-    // Parse the JSON response from the OpenAI API
-    const flashcards = JSON.parse(completion.choices[0].message.content)
-  
-    // Return the flashcards as a JSON response
-    return NextResponse.json(flashcards.flashcards)
+  try {
+    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const data = await req.text();
+    const completion = await model.generateContent([systemPrompt, ...data]);
+
+    let content = completion.response.text();
+      // completion.response.candidates[0].content.parts[0].text.trim();
+
+    content = content.replace(/```json/g, "").replace(/```/g, "");
+
+    const flashcards = JSON.parse(content);
+
+    return NextResponse.json(flashcards.flashcards);
+  } catch (e) {
+    console.error("Error generating flashcards:", e.message, e.stack);
+    return NextResponse.json([
+      {
+        front: "No flashcard generated",
+        back: "Please try a different topic.",
+      },
+    ]);
   }
+}
