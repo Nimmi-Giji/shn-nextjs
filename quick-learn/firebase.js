42425443f4dc23a7dd8d59fe3dd7f566d86dbaf0
gsk <gangaskumar83@gmail.com>
2024-10-05 20:42:35 +0530

    Correcting environment variables

--- quick-learn/firebase.js
+++ quick-learn/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: process.env.YOUR_API_KEY,
+    authDomain: process.env.YOUR_AUTH_DOMAIN,
+    projectId: process.env.YOUR_PROJECT_ID,
+    storageBucket: process.env.YOUR_STORAGE_BUCKET,
+    messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
+    appId: process.env.YOUR_APP_ID
 
 };
 
